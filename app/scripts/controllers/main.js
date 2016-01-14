eeaa7008d2b0fc430782cf900bf193ccea2ac47d
GarrethDottin <garrethdottin@gmail.com>
2016-01-13 19:54:16 -0500

    clean up of main file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,11 +12,11 @@ angular.module('angularImperativeCodebaseApp')
   	var vm = $scope,
   		rootUrl =  "http://localhost:3000/drones";
 
-  	vm.modelNames = [];
-  	vm.rowCollection = [];
-  	vm.displayCollection = [];
-  	vm.showModal = false;
-	allItems();
+    vm.modelNames = [];
+    vm.rowCollection = [];
+    vm.displayCollection = [];
+    vm.showModal = false;
+    allItems();
 
     vm.toggleModal = function(row){
         vm.showModal = !vm.showModal;
@@ -24,29 +24,32 @@ angular.module('angularImperativeCodebaseApp')
         vm.currentModelName = row.get('model_name');
     };
 
-	var anonymous = function (input) { return input };
-	var formatObj = function(input, drone) { 
-		var immutableObj = {model_name: input + drone.get('model_name'), model_number: drone.get('model_number')};
-		return Immutable.fromJS(immutableObj);
-	};
-	var modifyName = R.partial(formatObj, ["The Original "]);
+    var anonymous = function (input) { return input };
+
+    var formatObj = function(input, drone) { 
+    	var immutableObj = {model_name: input + drone.get('model_name'), 
+      model_number: drone.get('model_number')};
+    	return Immutable.fromJS(immutableObj);
+    };
+
+    var modifyName = R.partial(formatObj, ["The Original "]);
 
 
-	vm.parsedData = R.compose(anonymous, modifyName);
+  	vm.parsedData = R.compose(anonymous, modifyName);
 
-	function setTable (obj) {
-		var immutableData = Immutable.fromJS(obj.data); 
-		vm.rowCollection = immutableData.map(vm.parsedData);
-	}; 
+  	function setTable (obj) {
+  		var immutableData = Immutable.fromJS(obj.data); 
+  		vm.rowCollection = immutableData.map(vm.parsedData);
+  	}; 
 
 
-	//http calls 
-	vm.updateDetails = function (currentModelNumber, currentModelName) {
-  		var url = 'http://localhost:3000/drones/' + currentModelNumber;
-  		var data = {model_number: currentModelNumber, model_name: currentModelName};
-  		$http.post(url, data)
-  			.then(vm.toggleModal);
-  	};
+	  //http calls 
+  	vm.updateDetails = function (currentModelNumber, currentModelName) {
+    	var url = 'http://localhost:3000/drones/' + currentModelNumber;
+    	var data = {model_number: currentModelNumber, model_name: currentModelName};
+    	$http.post(url, data)
+    		.then(vm.toggleModal);
+    };
 
   	vm.createCall = function(modelName, modelNumber) {
   		var postUrl = 'http://localhost:3000/drones';
@@ -61,19 +64,17 @@ angular.module('angularImperativeCodebaseApp')
   			});
   	}; 
 
-	function firstDetailCall() {
-		var firstDetailedItem = "http://localhost:3000/drones/1";
-		return $http.get(firstDetailedItem).then(function(obj) { 
-			vm.detailCollection = Immutable.fromJS(obj.data);
-		});
-	}; 
+  	function firstDetailCall() {
+  		var firstDetailedItem = "http://localhost:3000/drones/1";
+  		return $http.get(firstDetailedItem).then(function(obj) { 
+  			vm.detailCollection = Immutable.fromJS(obj.data);
+  		});
+  	}; 
 
-	// All Items
-	function allItems () { 
-		return $http.get(rootUrl)
+  	// All Items
+  	function allItems () { 
+  		return $http.get(rootUrl)
   			.then(setTable)
   			.then(firstDetailCall);
-	}; 
-
-
+  	}; 
   });
